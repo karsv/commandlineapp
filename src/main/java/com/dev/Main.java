a618bdbcaf362ad5ca04ce79695a76f8af5f18c0
karsv <Pbvf2019!>
2020-02-26 14:25:08 +0200

    check travis

--- src/main/java/com/dev/Main.java
+++ src/main/java/com/dev/Main.java
@@ -4,6 +4,7 @@ import com.dev.service.SquareService;
 
 public class Main {
     public static void main(String[] args) {
+
         SquareService squareService = new SquareService(args[0]);
         System.out.println(squareService.getResult(args[1]));
     }
